27fdbc83bfe8ba2cb9d2307ebfef69b4923af9d5
MiralDalayka <miralthd2@gmail.com>
2025-01-28 15:06:18 +0300

    Working on Data layer : 1. Create POST fnuction on API service class : data layer 2. Create getSymbols Fucntion to fetch data for the main page request 3. Create request- response Body for Api

--- lib/data/web_services/api_service.dart
+++ lib/data/web_services/api_service.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
 import 'package:tradervolt_task/data/web_services/api_constants.dart';
+import 'package:tradervolt_task/data/web_services/data_requests_body/symbols_request_body.dart';
+import 'package:tradervolt_task/data/web_services/data_response_body/symbols_response_body.dart';
 
 part 'api_service.g.dart';
 
@@ -8,8 +10,8 @@ part 'api_service.g.dart';
 abstract class ApiService {
   factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
 
-  // @POST(ApiConstants.symbols)
-  // Future<SymbolsResponse> symbols(
-  //   @Body() SymbolsRequestBody symbolsRequestBody,
-  // );
+  @POST(ApiConstants.symbols)
+  Future<SymbolsResponse> getSymbols(
+    @Body() SymbolsRequest symbolsRequestBody,
+  );
 }
